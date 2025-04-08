a1a501d393ead953e7885085659ffa709457995f
Maicol Cortes <pgridex@gmail.com>
2025-04-07 21:12:46 -0500

    Mensaje descriptivo de los cambios

--- models.py
+++ models.py
@@ -1,24 +1,22 @@
-from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship
 from database import Base
 
-class Producto(Base):
-    __tablename__ = "productos"
+class Product(Base):
+    __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, index=True)
     descripcion = Column(String)
     precio = Column(Float)
-    categoria = Column(String)
+    # Si hay relación con inventarios u otras tablas, agregarlas aquí
+    inventory = relationship("Inventory", back_populates="producto")
 
-    inventario = relationship("Inventario", back_populates="producto")
-
-
-class Inventario(Base):
-    __tablename__ = "inventario"
+class Inventory(Base):
+    __tablename__ = "inventory"
 
     id = Column(Integer, primary_key=True, index=True)
-    producto_id = Column(Integer, ForeignKey("productos.id"))
+    producto_id = Column(Integer, index=True)
     cantidad = Column(Integer)
 
-    producto = relationship("Producto", back_populates="inventario")
+    producto = relationship("Product", back_populates="inventory")
